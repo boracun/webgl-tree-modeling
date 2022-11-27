cf1ed95e5ba5bb52929e42ab977692ea02f73f97
boracun <cun2424@gmail.com>
2022-11-28 01:53:29 +0300

    Add code for the base of the trunk

--- app.js
+++ app.js
@@ -22,7 +22,28 @@ let right = 1.5;
 let ytop = 1.5;
 let bottom = -1.5;
 
-let groundVertices = [];
+// Number of "faces" a tube will have. The more faces it has, the more round it looks.
+let faceCount = 50;
+
+// TODO: Need to separate trunk and ground vertices later
+let tubeVertices = [];
+
+function addGroundVertices() {
+    tubeVertices.push(vec4(-1.0, 0.0, -1.0, 1.0));
+    tubeVertices.push(vec4(1.0, 0.0, -1.0, 1.0));
+    tubeVertices.push(vec4(-1.0, 0.0, 1.0, 1.0));
+    tubeVertices.push(vec4(1.0, 0.0, 1.0, 1.0));
+}
+
+function addTubeVertices(innerRadius, outerRadius, height) {
+    for (let i = 0; i < faceCount; i++) {
+        tubeVertices.push(vec4(outerRadius * Math.sin(radians(i * 360 / faceCount)), 0.0, outerRadius * Math.cos(radians(i * 360 / faceCount)), 1.0));
+        tubeVertices.push(vec4(innerRadius * Math.sin(radians(i * 360 / faceCount)), height, innerRadius * Math.cos(radians(i * 360 / faceCount)), 1.0));
+    }
+
+    tubeVertices.push(vec4(outerRadius * Math.sin(0), 0.0, outerRadius * Math.cos(0), 1.0));
+    tubeVertices.push(vec4(innerRadius * Math.sin(0), height, innerRadius * Math.cos(0), 1.0));
+}
 
 window.onload = function init() {
     let increaseCameraAngleButton = document.getElementById("increase-camera-angle-button");
@@ -46,10 +67,8 @@ window.onload = function init() {
     glTube.clearColor(0.53, 0.81, 0.94, 1.0);
     glTube.enable(glTube.DEPTH_TEST);
 
-    groundVertices.push(vec4(-1.0, 0.0, -1.0, 1.0));
-    groundVertices.push(vec4(1.0, 0.0, -1.0, 1.0));
-    groundVertices.push(vec4(-1.0, 0.0, 1.0, 1.0));
-    groundVertices.push(vec4(1.0, 0.0, 1.0, 1.0));
+    addGroundVertices();
+    addTubeVertices(0.1, 0.2, 0.15);
 
     // Load shaders and initialize attribute buffers
     programTube = initShaders(glTube, "vertex-shader", "fragment-shader");
@@ -65,7 +84,7 @@ window.onload = function init() {
 
     vBufferTube = glTube.createBuffer();
     glTube.bindBuffer(glTube.ARRAY_BUFFER, vBufferTube);
-    glTube.bufferData(glTube.ARRAY_BUFFER, flatten(groundVertices), glTube.STATIC_DRAW);
+    glTube.bufferData(glTube.ARRAY_BUFFER, flatten(tubeVertices), glTube.STATIC_DRAW);
 
     let vPositionTube = glTube.getAttribLocation(programTube, "vPosition");
     glTube.vertexAttribPointer(vPositionTube, 4, glTube.FLOAT, false, 0, 0);
@@ -85,7 +104,7 @@ function render() {
 
     // Change drawing color to brown and draw the rest
     glTube.uniform1i(glTube.getUniformLocation(programTube, "green"), 0);
-    glTube.drawArrays(glTube.TRIANGLE_STRIP, 4, 100);     // TODO: Set the count
+    glTube.drawArrays(glTube.TRIANGLE_STRIP, 4, 1000);     // TODO: Set the count
 
     requestAnimFrame(render);
 }
