2a5b6f3f06d9ea4ddebcbb8bc2505370b3276aa8
boracun <cun2424@gmail.com>
2022-11-28 19:10:34 +0300

    Change some variable names

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 let canvas;
-let glTube;
-let programTube;
+let gl;
+let program;
 
 let instanceMatrix;     // TODO: Figure out what this is for
 let projectionMatrix;
 let modelViewMatrix;
 
 // Add color buffer if needed later
-let vBufferTube;
+let vBuffer;
 
 // For model-view matrix
 let cameraAngle = 45
@@ -73,8 +73,8 @@ function addConeVertices(radius, height) {
 
 function drawGround() {
     // Change drawing color to green and draw the ground
-    glTube.uniform1i(glTube.getUniformLocation(programTube, "green"), 1);
-    glTube.drawArrays(glTube.TRIANGLE_STRIP, 0, groundVertexCount);
+    gl.uniform1i(gl.getUniformLocation(program, "green"), 1);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, groundVertexCount);
 }
 
 // Bottom tube has length: baseTubeLength
@@ -84,28 +84,28 @@ function drawTrunk() {
     let trunkTransformationMatrix;  // This is used when we want to scale an object but not want to save it in the stack
 
     // Change drawing color to brown and draw the rest
-    glTube.uniform1i(glTube.getUniformLocation(programTube, "green"), 0);
+    gl.uniform1i(gl.getUniformLocation(program, "green"), 0);
 
     // Bottom tube
-    glTube.drawArrays(glTube.TRIANGLE_STRIP, groundVertexCount, tubeVertexCount);
+    gl.drawArrays(gl.TRIANGLE_STRIP, groundVertexCount, tubeVertexCount);
 
     // Middle tube
     modelViewMatrix = mult(modelViewMatrix, translate(0, baseTubeLength, 0));
     trunkTransformationMatrix = mult(modelViewMatrix, scale(10 / 17, 2, 10 / 17));
-    glTube.uniformMatrix4fv(glTube.getUniformLocation(programTube, "modelViewMatrix"), false, flatten(trunkTransformationMatrix));
-    glTube.drawArrays(glTube.TRIANGLE_STRIP, groundVertexCount, tubeVertexCount);
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelViewMatrix"), false, flatten(trunkTransformationMatrix));
+    gl.drawArrays(gl.TRIANGLE_STRIP, groundVertexCount, tubeVertexCount);
 
     // Top tube
     modelViewMatrix = mult(modelViewMatrix, translate(0, 2 * baseTubeLength, 0));
     trunkTransformationMatrix = mult(modelViewMatrix, scale(Math.pow(10 / 17, 2), 3, Math.pow(10 / 17, 2)));
-    glTube.uniformMatrix4fv(glTube.getUniformLocation(programTube, "modelViewMatrix"), false, flatten(trunkTransformationMatrix));
-    glTube.drawArrays(glTube.TRIANGLE_STRIP, groundVertexCount, tubeVertexCount);
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelViewMatrix"), false, flatten(trunkTransformationMatrix));
+    gl.drawArrays(gl.TRIANGLE_STRIP, groundVertexCount, tubeVertexCount);
 
     // Cone on the top
     modelViewMatrix = mult(modelViewMatrix, translate(0, 3 * baseTubeLength, 0));
     trunkTransformationMatrix = mult(modelViewMatrix, scale(Math.pow(10 / 17, 3), 6, Math.pow(10 / 17, 3)));
-    glTube.uniformMatrix4fv(glTube.getUniformLocation(programTube, "modelViewMatrix"), false, flatten(trunkTransformationMatrix));
-    glTube.drawArrays(glTube.TRIANGLE_FAN, groundVertexCount + tubeVertexCount, coneVertexCount);
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelViewMatrix"), false, flatten(trunkTransformationMatrix));
+    gl.drawArrays(gl.TRIANGLE_FAN, groundVertexCount + tubeVertexCount, coneVertexCount);
 }
 
 function drawLimb(rotationMatrix, length, depth) {
@@ -114,8 +114,8 @@ function drawLimb(rotationMatrix, length, depth) {
     modelViewMatrix = mult(modelViewMatrix, rotationMatrix);
     limbTransformationMatrix = mult(modelViewMatrix, scale(Math.pow(10 / 17, depth), length, Math.pow(10 / 17, depth)))
 
-    glTube.uniformMatrix4fv(glTube.getUniformLocation(programTube, "modelViewMatrix"), false, flatten(limbTransformationMatrix));
-    glTube.drawArrays(glTube.TRIANGLE_STRIP, groundVertexCount, tubeVertexCount);
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelViewMatrix"), false, flatten(limbTransformationMatrix));
+    gl.drawArrays(gl.TRIANGLE_STRIP, groundVertexCount, tubeVertexCount);
 
     modelViewMatrix = mult(modelViewMatrix, translate(0, length * baseTubeLength, 0));
 }
@@ -135,12 +135,12 @@ window.onload = function init() {
 
     canvas = document.getElementById("gl-canvas");
 
-    glTube = WebGLUtils.setupWebGL(canvas);
-    if (!glTube) {alert("WebGL isn't available");}
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) {alert("WebGL isn't available");}
 
-    glTube.viewport(0, 0, canvas.width, canvas.height);
-    glTube.clearColor(0.53, 0.81, 0.94, 1.0);
-    glTube.enable(glTube.DEPTH_TEST);
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0.53, 0.81, 0.94, 1.0);
+    gl.enable(gl.DEPTH_TEST);
 
     // Add needed vertices
     addGroundVertices();
@@ -148,32 +148,32 @@ window.onload = function init() {
     addConeVertices(0.17, 0.02);
 
     // Load shaders and initialize attribute buffers
-    programTube = initShaders(glTube, "vertex-shader", "fragment-shader");
-    glTube.useProgram(programTube);
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
 
     instanceMatrix = mat4();
 
     projectionMatrix = ortho(left, right, bottom, ytop, near, far);
     modelViewMatrix = mat4();
 
-    glTube.uniformMatrix4fv(glTube.getUniformLocation(programTube, "modelViewMatrix"), false, flatten(modelViewMatrix));
-    glTube.uniformMatrix4fv(glTube.getUniformLocation(programTube, "projectionMatrix"), false, flatten(projectionMatrix));
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelViewMatrix"), false, flatten(modelViewMatrix));
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "projectionMatrix"), false, flatten(projectionMatrix));
 
-    vBufferTube = glTube.createBuffer();
-    glTube.bindBuffer(glTube.ARRAY_BUFFER, vBufferTube);
-    glTube.bufferData(glTube.ARRAY_BUFFER, flatten(vertices), glTube.STATIC_DRAW);
+    vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
 
-    let vPositionTube = glTube.getAttribLocation(programTube, "vPosition");
-    glTube.vertexAttribPointer(vPositionTube, 4, glTube.FLOAT, false, 0, 0);
-    glTube.enableVertexAttribArray(vPositionTube);
+    let vPositionTube = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPositionTube, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPositionTube);
 
     render();
 }
 
 function render() {
-    glTube.clear(glTube.COLOR_BUFFER_BIT | glTube.DEPTH_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     modelViewMatrix = lookAt(eye, at, up);
-    glTube.uniformMatrix4fv(glTube.getUniformLocation(programTube, "modelViewMatrix"), false, flatten(modelViewMatrix));
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, "modelViewMatrix"), false, flatten(modelViewMatrix));
 
     ctmStack = [mat4()];
     drawGround();
